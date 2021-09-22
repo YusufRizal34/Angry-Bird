dd67f9981420f8459b0fa3caf95861c1f5613704
Muhammad Yusuf Rizal <myusufrizal34@gmail.com>
2021-09-22 21:20:37 +0700

    Renew game build

--- Assets/Scripts/BrownBird.cs
+++ Assets/Scripts/BrownBird.cs
@@ -4,14 +4,15 @@ using UnityEngine;
 
 public class BrownBird : Bird
 {
-    [SerializeField]
-    public float _explodeForce = 10;
+    [SerializeField] private float _explodeForce = 10;
+    [SerializeField] private GameObject explosion;
     // public bool _hasExplode = false;
 
     public override void OnCollisionEnter2D(Collision2D col)
     {
         if(col.transform.GetComponent<Rigidbody2D>()){
             col.rigidbody.AddForce(col.transform.position * _explodeForce);
+            Instantiate(explosion, col.transform);
         }
         Destroy(gameObject);
     }
